da96799f88065c502fb8d14f9c60052993ff0b9c
Leandro Alves <leandro.euladio@gmail.com>
2021-12-17 16:28:00 +0000

    Init

---
+++ src/main/java/Utilizador.java
@@ -0,0 +1,56 @@
+public class Utilizador {
+
+    String id;
+    String nome;
+    String morada;
+    String userName;
+    String password;
+
+    public Utilizador(String id, String nome, String morada, String userName, String password) {
+        this.id = id;
+        this.nome = nome;
+        this.morada = morada;
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getMorada() {
+        return morada;
+    }
+
+    public void setMorada(String morada) {
+        this.morada = morada;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
